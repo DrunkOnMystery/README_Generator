374a928eecf7e386d38de34ec128795bf4d96f7a
Ben Brumby <bbrumby00@gmail.com>
2020-10-01 10:00:02 -0400

    looks like the generator is mostly working now

--- Develop/index.js
+++ Develop/index.js
@@ -10,11 +10,50 @@ inquirer.prompt([
     },
     {
         type: "input",
-        message: "Enter Description",
+        message: "Enter Description of the Project",
         name: "description"
+    },
+    {
+        type: "input",
+        message: "Describe the steps necessary for installing this Project",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Describe the proper usage of this project",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "List the credits for your project",
+        name: "credits"
+    },
+    {
+        type: "input",
+        message: "List any relevant licensing information",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Any badges you need to list, do so here:",
+        name: "badges"
+    },
+    {
+        type: "input",
+        message: "List any contributions made to this project",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "Do you have any run tests for your project? If so, put them here.",
+        name: "runTest"
     }
 ])
 
 .then(function(answers){
     console.log(answers);
+
+    const generatedFile = generateMarkdown(answers);
+
+    console.log(generatedFile);
 })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,58 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+return `
+# ${answers.title}
 
-`;
+## Description 
+
+${answers.description}
+
+## Table of Contents (Optional)
+
+If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+* [Contributing](#contributions)
+* [Tests](#runTest)
+
+
+## Installation
+
+${answers.installation}
+
+
+## Usage 
+
+${answers.usage}
+
+
+## Credits
+
+${answers.credits}
+
+
+## License
+
+${answers.license}
+
+## Badges
+
+${answers.badges}
+
+## Contributing
+
+${answers.contributions}
+
+## Tests
+
+${answers.runTest}`
+
+;
 }
 
 module.exports = generateMarkdown;
+
